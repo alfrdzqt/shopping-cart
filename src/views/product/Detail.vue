95778b77cb6eff2a78d19dc1100d301b52eb11a2
SYERA AL-FARIDZI <alfrdzi1203@gmail.com>
2024-03-13 03:37:29 +0800

    Update Vue router to use product, product-detail, and cart components

---
+++ src/views/product/Detail.vue
@@ -0,0 +1,12 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div>
+    <h1>Product Detail</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
